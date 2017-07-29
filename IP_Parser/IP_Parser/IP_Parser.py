d51bf42231d636fbec7a6f8c863cba236ea74ec7
Jake Pringle <Jake.B.Pringle@gmail.com>
2017-07-30 00:48:57 +1000

    Cleaned up JSON output
    JSON parser now outputs in order of appearance in spreadsheet.

--- IP_Parser/IP_Parser/IP_Parser.py
+++ IP_Parser/IP_Parser/IP_Parser.py
@@ -5,13 +5,7 @@ class IP_Parser(object):
         pass
 
     def Parse(self):
-        #open file
-        #check if already open, else break
-        #create dict with keys for each column, and the associated values 
-        #each dict key will the name from row one for each respective column
-        #each value in the same column will be in a listz   
-
-        file = open('../../Data/ipgod101.csv','r')
+        file = open('../../Data/ipgod101_SmallSizeSample.csv','r')
 
         ipgod = []
         read_lines = []
@@ -22,36 +16,34 @@ class IP_Parser(object):
 
         #split up top line wth comma ',' as delimter
         keys=keys.rstrip().split(',')
-        
-        #ipgod.append(keys)
-        #then loop each element in the "keys" list so that each new 
-        for key in keys:
-            pass
 
+        #read in data and split with comma ',' as delimeter.
         for line in file:
             line = line.rstrip().split(',')
-            read_lines.append(line)
+            read_lines.insert(0,line)
 
+        #for every key, pop the data in the same column and append
         for key in keys:
             temp_list = []
             for line in read_lines:
                 if not line:
                     break
                 temp_list.insert(0, line.pop(0))
-            ipgod.insert(0,(key, temp_list))
+            ipgod.append((key, temp_list))
         b = dict(ipgod)
-        with open('../../Data/ipgod101JSON.json', 'w') as outfile:
+
+        with open('../../Data/ipgod101_SmallSampleSize.json', 'w') as outfile:
             json.dump(b, outfile)
 
     def Read_In_JSON(self):
         ipgod = {}
         with open('../../Data/ipgod101JSON.json', 'r') as infile:
             ipgod = json.load(infile)
-            
+        
         pass
 
 
 if __name__ == '__main__':
     p = IP_Parser()
-    #p.Parse()
-    p.Read_In_JSON()
+    p.Parse()
+    #p.Read_In_JSON()
