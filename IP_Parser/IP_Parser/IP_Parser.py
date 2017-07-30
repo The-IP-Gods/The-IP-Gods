e113873fe9db372906c8b4f5b9755b5deabfef9a
Jake Pringle <Jake.B.Pringle@gmail.com>
2017-07-30 15:45:19 +1000

    Zipped JSON, removed Readme folder and updated gitignore
    Tidied up parsing code, removed /Data folder from .gitignore and zipped
    parsed JSON ipgod data.

--- IP_Parser/IP_Parser/IP_Parser.py
+++ IP_Parser/IP_Parser/IP_Parser.py
@@ -2,12 +2,12 @@ import json
 
 class IP_Parser(object):
     def __init__(self, filename=None):
-        pass
+        self.ipgod = {}
 
     def Parse(self):
-        file = open('../../Data/ipgod101_SmallSizeSample.csv','r')
+        file = open('../../Data/ipgod107.csv','r')
 
-        ipgod = []
+        temp_ipgod = []
         read_lines = []
 
         ###setting up dict keys
@@ -29,21 +29,41 @@ class IP_Parser(object):
                 if not line:
                     break
                 temp_list.insert(0, line.pop(0))
-            ipgod.append((key, temp_list))
-        b = dict(ipgod)
+            temp_ipgod.append((key, temp_list))
+        self.ipgod = dict(temp_ipgod)
 
-        with open('../../Data/ipgod101_SmallSampleSize.json', 'w') as outfile:
-            json.dump(b, outfile)
+        with open('../../Data/ipgod107.json', 'w') as outfile:
+            json.dump(self.ipgod, outfile)
 
     def Read_In_JSON(self):
-        ipgod = {}
-        with open('../../Data/ipgod101JSON.json', 'r') as infile:
-            ipgod = json.load(infile)
-        
-        pass
+        with open('../../Data/ipgod107.json', 'r') as infile:
+            self.ipgod = json.load(infile)
+
+    def Search_JSON(self):
+        #print keys to select from
+        #select key and allow search
+        #show results
+        ###Not implemented yet
+
+        ipgod_search = self.ipgod.copy()
+        search_query = ''
+
+        self.Read_In_JSON()
+        x=1
+        keys = list(self.ipgod.keys())
+        for key in keys:
+            print ('{}.'.format(x), key)
+            x=x+1
+        cat_search_num = input('Search #: ')
+        cat_search = keys[(int(cat_search_num))-1]
+        search_query = input('Search query: ')
+        print(list(self.ipgod.values()))
+
+        #if search_query in the category /= sear_query, remove
 
 
 if __name__ == '__main__':
     p = IP_Parser()
     p.Parse()
     #p.Read_In_JSON()
+    #p.Search_JSON()
