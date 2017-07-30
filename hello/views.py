b70c4b3f31c71fe4fd4eea278a8c63ebbdd4de33
Riley Baird <riley@openmailbox.org>
2017-07-30 10:24:13 +1000

    Added non-working commit for Video

--- hello/views.py
+++ hello/views.py
@@ -10,7 +10,6 @@ import sys
 
 # Create your views here.
 def index(request):
-    # return render(request, 'index.html')
     results = []
     csv.field_size_limit(sys.maxsize)
     if request.method == 'POST':
@@ -19,18 +18,27 @@ def index(request):
             reader = csv.reader(open('hello/static/ipgod107.csv'))
             for line in reader:
                 if line[0] == form.cleaned_data['your_name']:
+                    if form.cleaned_data['email'] != "":
+                        AppNums.objects.create(email=form.cleaned_data['email'], appnums=form.cleaned_data['your_name']).save()
+                        return HttpResponse('The status of application '+line[0]+' is '+line[9]+'. You will receive updates at your email address, '+form.cleaned_data['email'])
                     return HttpResponse('The status of application '+line[0]+' is '+line[9])
             return HttpResponse('Sorry, but we couldn\'t find that application number in our database.')
         elif form.is_valid and form.cleaned_data['keywords'] != "":
-            reader = csv.reader(open('hello/static/ipgod122a.csv'))
-            for line in reader:
+            if form.cleaned_data['email'] != "":
+                Keywords.objects.create(email=form.cleaned_data['email'], keywords=form.cleaned_data['keywords']).save()
+            reader122a = csv.reader(open('hello/static/ipgod122a.csv'))
+            for line in reader122a:
                 if form.cleaned_data['keywords'] in line[1]:
-                    results.append((line[0],line[1]))
-            return render(request, 'index.html', {'results': results})
+                    reader107 = csv.reader(open('hello/static/ipgod107.csv'))
+                    for line107 in reader107:
+                        if line[0] == line107[0]:
+                            status = line107[9]
+                            category = line107[6]
+                    results.append((line[0],line[1],line107[9],line107[6]))
+            return render(request, 'results.html', {'results': results})
     else:
         form = NameForm()
     return render(request, 'index.html', {'form': form})
-
 
 def db(request):
 
@@ -58,10 +66,15 @@ def form(request):
         elif form.is_valid and form.cleaned_data['keywords'] != "":
             if form.cleaned_data['email'] != "":
                 Keywords.objects.create(email=form.cleaned_data['email'], keywords=form.cleaned_data['keywords']).save()
-            reader = csv.reader(open('hello/static/ipgod122a.csv'))
-            for line in reader:
+            reader122a = csv.reader(open('hello/static/ipgod122a.csv'))
+            for line in reader122a:
                 if form.cleaned_data['keywords'] in line[1]:
-                    results.append((line[0],line[1]))
+                    reader107 = csv.reader(open('hello/static/ipgod107.csv'))
+                    for line107 in reader107:
+                        if line[0] == line107[0]:
+                            status = line107[9]
+                            category = line107[6]
+                    results.append((line[0],line[1],line107[9],line107[6]))
             return render(request, 'results.html', {'results': results})
     else:
         form = NameForm()
